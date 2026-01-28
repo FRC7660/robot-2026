d90434a3bba8e7b0b4f8f2d253f3d3a32beb2159
karmikhael <solomoncarmichael@gmail.com>
2026-01-27 20:11:00 -0500

    initial turret test

---
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+public final class Turret {
+  private Turret() {}
+
+  public static Translation2d getTarget(Translation2d turretPosition) {
+    // Implementation to get target based on turret position
+    return new Translation2d(); // Placeholder return
+  }
+  // Prevent instantiation
+}
