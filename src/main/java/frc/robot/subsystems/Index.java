51b170f80cd4c63bf8e93e3a768e1fb0b7bc1b9c
sdecamillo <sdecamillo@fgrhs.org>
2026-01-31 11:33:24 -0500

    Index base level config

--- src/main/java/frc/robot/subsystems/Index.java
+++ src/main/java/frc/robot/subsystems/Index.java
@@ -1,17 +1,45 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.*;
-// import com.revrobotics.config.*;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 // initializes index and funnel motors
 public class Index extends SubsystemBase {
   private final SparkMax indexMotor =
-      new SparkMax(Constants.IndexConstants.INDEX_MOTOR_ID, Constants.IndexConstants.INDEX_TYPE);
+      new SparkMax(Constants.Index.INDEX_MOTOR_ID, Constants.Index.INDEX_TYPE);
 
   private final SparkMax funnelMotor =
-      new SparkMax(Constants.IndexConstants.FUNNEL_MOTOR_ID, Constants.IndexConstants.FUNNEL_TYPE);
+      new SparkMax(Constants.Index.FUNNEL_MOTOR_ID, Constants.Index.FUNNEL_TYPE);
+
+  public Index() {
+    // Configure motors with settings from Constants
+    SparkMaxConfig indexConfig = configureIndexMotor();
+    SparkMaxConfig funnelConfig = configureFunnelMotor();
+
+    indexMotor.configure(
+        indexConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    funnelMotor.configure(
+        funnelConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  // Index Motor Config
+  public SparkMaxConfig configureIndexMotor() {
+    SparkMaxConfig indexConfig = new SparkMaxConfig();
+    indexConfig.idleMode(SparkBaseConfig.IdleMode.kBrake).inverted(false);
+    return indexConfig;
+  }
+
+  // Funnel Motor Config
+  public SparkMaxConfig configureFunnelMotor() {
+    SparkMaxConfig funnelConfig = new SparkMaxConfig();
+    funnelConfig.idleMode(SparkBaseConfig.IdleMode.kBrake).inverted(false);
+    return funnelConfig;
+  }
 
   public void setIndexSpeed(double speed) {
     this.indexMotor.set(speed);
