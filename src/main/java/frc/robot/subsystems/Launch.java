3b1cdbc8b7c47e42d9edc3633f74f57cf3d3a295
sdecamillo <sdecamillo@fgrhs.org>
2026-02-13 09:05:25 -0500

    close to done launch code

--- src/main/java/frc/robot/subsystems/Launch.java
+++ src/main/java/frc/robot/subsystems/Launch.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems; 
 
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
@@ -8,6 +8,17 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
 import frc.robot.Constants;
 
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.ArmConfig;
+import yams.mechanisms.positional.Arm;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.remote.TalonFXWrapper;
+
 public class Launch {
   private final SparkFlex motor1 =
       new SparkFlex(Constants.Launch.MOTOR1_ID, SparkLowLevel.MotorType.kBrushless);
@@ -15,23 +26,42 @@ public class Launch {
   private final SparkFlex motor2 =
       new SparkFlex(Constants.Launch.MOTOR2_ID, SparkLowLevel.MotorType.kBrushless);
 
-  public Launch() {
-    SparkFlexConfig baseConfig = new SparkFlexConfig();
-    baseConfig.idleMode(IdleMode.kCoast);
+  SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
+  .withControlMode(ControlMode.CLOSED_LOOP)
+  // Feedback Constants (PID Constants)
+  .withClosedLoopController(50, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+  .withSimClosedLoopController(50, 0, 0, DegreesPerSecond.of(90),  DegreesPerSecondPerSecond.of(45))
+  // Feedforward Constants
+  .withFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+  .withSimFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+  // Telemetry name and verbosity level
+  .withTelemetry("ShooterMotor", TelemetryVerbosity.HIGH)
+  // Gearing from the motor rotor to final shaft.
+  // In this example gearbox(3,4) is the same as gearbox("3:1","4:1") which corresponds to the gearbox attached to your motor.
+  .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+  // Motor properties to prevent over currenting.
+  .withMotorInverted(false)
+  .withIdleMode(MotorMode.COAST)
+  .withStatorCurrentLimit(Amps.of(40))
+  .withClosedLoopRampRate(Seconds.of(0.25))
+  .withOpenLoopRampRate(Seconds.of(0.25));
 
-    motor1.configure(
-        new SparkFlexConfig().apply(baseConfig).inverted(true),
-        ResetMode.kResetSafeParameters,
-        PersistMode.kPersistParameters);
+// Vendor motor controller object
+SparkMax spark = new SparkMax(4, MotorType.kBrushless);
 
-    motor2.configure(
-        new SparkFlexConfig().apply(baseConfig).inverted(false),
-        ResetMode.kResetSafeParameters,
-        PersistMode.kPersistParameters);
-  }
+// Create our SmartMotorController from our Spark and config with the NEO.
+SmartMotorController sparkSmartMotorController = new SparkWrapper(spark, DCMotor.getNEO(1), smcConfig);
 
-  public void setSpeed(double speed) {
-    this.motor1.set(speed);
-    this.motor2.set(speed);
-  }
+FlyWheelConfig shooterConfig = new FlyWheelConfig(motor)
+  // Diameter of the flywheel.
+  .withDiameter(Inches.of(4))
+  // Mass of the flywheel.
+  .withMass(Pounds.of(1))
+  // Maximum speed of the shooter.
+  .withUpperSoftLimit(RPM.of(1000))
+  // Telemetry name and verbosity for the arm.
+  .withTelemetry("Shooter", TelemetryVerbosity.HIGH);
+
+  // Shooter Mechanism
+  private FlyWheel shooter = new FlyWheel(shooterConfig);
 }
