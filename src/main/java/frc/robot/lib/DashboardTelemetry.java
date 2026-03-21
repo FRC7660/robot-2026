ae7610a0e3dd9128719891642d139ce3f8599690
Scott Moser <smoser@brickies.net>
2026-03-21 12:26:05 -0400

    Make the DashboardTelemetry also log.

--- src/main/java/frc/robot/lib/DashboardTelemetry.java
+++ src/main/java/frc/robot/lib/DashboardTelemetry.java
@@ -2,6 +2,7 @@ package frc.robot.lib;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
+import org.littletonrobotics.junction.Logger;
 
 public final class DashboardTelemetry {
   private DashboardTelemetry() {}
@@ -11,18 +12,21 @@ public final class DashboardTelemetry {
   }
 
   public static void putBoolean(String key, boolean value) {
+    Logger.recordOutput(key, value);
     if (isHighVerbosity()) {
       SmartDashboard.putBoolean(key, value);
     }
   }
 
   public static void putNumber(String key, double value) {
+    Logger.recordOutput(key, value);
     if (isHighVerbosity()) {
       SmartDashboard.putNumber(key, value);
     }
   }
 
   public static void putString(String key, String value) {
+    Logger.recordOutput(key, value);
     if (isHighVerbosity()) {
       SmartDashboard.putString(key, value);
     }
